b487212207a775a5dcc99b51472eb7ebdd2c1ed0
Vyacheslav Ikonnikov <vyacheslav.ikonnikov@cyberia.studio>
2023-09-10 18:33:25 +0700

    fix

--- plugins/tilt.ts
+++ plugins/tilt.ts
@@ -10,7 +10,7 @@ export default defineNuxtPlugin((nuxtApp) => {
                 }, 500);
             }
 
-            const startTilt = (e: MouseEvent) => {
+            const startTilt = () => {
                 setTransition();
             };
 
